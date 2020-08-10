2a88b2e4e0fe7bdc3e799574eaba7c12b226c711
wardensdev <69459114+wardensdev@users.noreply.github.com>
2020-08-10 06:10:11 -0400

    Add files via upload

--- Camera3D.gd
+++ Camera3D.gd
@@ -2,7 +2,7 @@ extends Camera3D
 
 
 func _process(delta):
-	if Input.is_action_pressed("forward"):
+	if Input.is_action_pressed("ui_up"):
 		translation.z += .2
-	if Input.is_action_pressed("backward"):
+	if Input.is_action_pressed("ui_down"):
 		translation.z -= .2
